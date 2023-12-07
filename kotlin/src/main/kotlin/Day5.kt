bff5b89f700e082ffd3eb960363eda29a0b3784c
Mathias Langlet <mathias.langlet@sj.se>
2023-12-07 16:55:50 +0100

    Generalize day 5 input parsing

--- kotlin/src/main/kotlin/Day5.kt
+++ kotlin/src/main/kotlin/Day5.kt
@@ -3,6 +3,7 @@ import java.nio.file.Path
 import java.util.regex.Pattern
 
 val digit = Pattern.compile("\\d+")
+val mappingHeading = Pattern.compile("(.*)\\smap:")
 
 fun main() {
     val input = Files.readAllLines(Path.of("input/day5/day5.txt"))
@@ -10,28 +11,14 @@ fun main() {
     lateinit var partOneSeeds: List<Long>
     lateinit var partTwoSeeds: List<LongRange>
 
-    var parsingSeedToSoil = false
-    val seedToSoil = ArrayList<MappingRange>()
-
-    var parsingSoilToFertilizer = false
-    val soilToFertilizer = ArrayList<MappingRange>()
-
-    var parsingFertilizerToWater = false
-    val fertilizerToWater = ArrayList<MappingRange>()
-
-    var parsingWaterToLight = false
-    val waterToLight = ArrayList<MappingRange>()
-
-    var parsingLightToTemperature = false
-    val lightToTemperature = ArrayList<MappingRange>()
-
-    var parsingTemperatureToHumidity = false
-    val temperatureToHumidity = ArrayList<MappingRange>()
-
-    var parsingHumidityToLocation = false
-    val humidityToLocation = ArrayList<MappingRange>()
+    val mappings = HashMap<String, ArrayList<MappingRange>>()
+    lateinit var mappingName: String
 
     input.forEach {
+        if (it.isBlank()){
+            return@forEach
+        }
+
         if (it.startsWith("seeds:")) {
             partOneSeeds = it
                 .split(":")[1]
@@ -41,86 +28,55 @@ fun main() {
                 .toList()
 
             partTwoSeeds = parseSeeds(it)
+
+            return@forEach
         }
 
-        if (it.startsWith("seed-to-soil")) {
-            parsingSeedToSoil = true
-        } else if (parsingSeedToSoil && it.isBlank()) {
-            parsingSeedToSoil = false
-        } else if (parsingSeedToSoil) {
-            seedToSoil.add(buildMappingRange(it))
-        }
-
-        if (it.startsWith("soil-to-fertilizer")) {
-            parsingSoilToFertilizer = true
-        } else if (parsingSoilToFertilizer && it.isBlank()) {
-            parsingSoilToFertilizer = false
-        } else if (parsingSoilToFertilizer) {
-            soilToFertilizer.add(buildMappingRange(it))
-        }
-
-        if (it.startsWith("fertilizer-to-water")) {
-            parsingFertilizerToWater = true
-        } else if (parsingFertilizerToWater && it.isBlank()) {
-            parsingFertilizerToWater = false
-        } else if (parsingFertilizerToWater) {
-            fertilizerToWater.add(buildMappingRange(it))
-        }
-
-        if (it.startsWith("water-to-light")) {
-            parsingWaterToLight = true
-        } else if (parsingWaterToLight && it.isBlank()) {
-            parsingWaterToLight = false
-        } else if (parsingWaterToLight) {
-            waterToLight.add(buildMappingRange(it))
-        }
-
-        if (it.startsWith("light-to-temperature")) {
-            parsingLightToTemperature = true
-        } else if (parsingLightToTemperature && it.isBlank()) {
-            parsingLightToTemperature = false
-        } else if (parsingLightToTemperature) {
-            lightToTemperature.add(buildMappingRange(it))
-        }
-
-        if (it.startsWith("temperature-to-humidity")) {
-            parsingTemperatureToHumidity = true
-        } else if (parsingTemperatureToHumidity && it.isBlank()) {
-            parsingTemperatureToHumidity = false
-        } else if (parsingTemperatureToHumidity) {
-            temperatureToHumidity.add(buildMappingRange(it))
-        }
-
-        if (it.startsWith("humidity-to-location")) {
-            parsingHumidityToLocation = true
-        } else if (parsingHumidityToLocation && it.isBlank()) {
-            parsingHumidityToLocation = false
-        } else if (parsingHumidityToLocation) {
-            humidityToLocation.add(buildMappingRange(it))
+        val mappingMatcher = mappingHeading.matcher(it)
+        if(mappingMatcher.find()){
+            mappingName = mappingMatcher.group(1)
+            mappings[mappingName] = ArrayList()
+        } else {
+            mappings[mappingName]!!.add(buildMappingRange(it))
         }
     }
 
     val partOne = partOneSeeds.minOf { seed ->
-        val soil = matchRange(seed, seedToSoil)
-        val fertilizer = matchRange(soil, soilToFertilizer)
-        val water = matchRange(fertilizer, fertilizerToWater)
-        val light = matchRange(water, waterToLight)
-        val temperature = matchRange(light, lightToTemperature)
-        val humidity = matchRange(temperature, temperatureToHumidity)
-        matchRange(humidity, humidityToLocation)
+        matchRange(
+            matchRange(
+                matchRange(
+                    matchRange(
+                        matchRange(
+                            matchRange(
+                                matchRange(seed,
+                                    mappings["seed-to-soil"]!!),
+                                mappings["soil-to-fertilizer"]!!),
+                            mappings["fertilizer-to-water"]!!),
+                        mappings["water-to-light"]!!),
+                    mappings["light-to-temperature"]!!),
+                mappings["temperature-to-humidity"]!!),
+            mappings["humidity-to-location"]!!)
     }
 
-    val partTwo = partTwoSeeds.minOf {
-        it.minOf { seed ->
-            val soil = matchRange(seed, seedToSoil)
-            val fertilizer = matchRange(soil, soilToFertilizer)
-            val water = matchRange(fertilizer, fertilizerToWater)
-            val light = matchRange(water, waterToLight)
-            val temperature = matchRange(light, lightToTemperature)
-            val humidity = matchRange(temperature, temperatureToHumidity)
-            matchRange(humidity, humidityToLocation)
-        }
-    }
+    val partTwo = 0
+//    val partTwo = partTwoSeeds.minOf {
+//        it.minOf { seed ->
+//            matchRange(
+//                matchRange(
+//                    matchRange(
+//                        matchRange(
+//                            matchRange(
+//                                matchRange(
+//                                    matchRange(seed,
+//                                        mappings["seed-to-soil"]!!),
+//                                    mappings["soil-to-fertilizer"]!!),
+//                                mappings["fertilizer-to-water"]!!),
+//                            mappings["water-to-light"]!!),
+//                        mappings["light-to-temperature"]!!),
+//                    mappings["temperature-to-humidity"]!!),
+//                mappings["humidity-to-location"]!!)
+//        }
+//    }
 
     println("Part one: $partOne")
     println("Part two: $partTwo")
